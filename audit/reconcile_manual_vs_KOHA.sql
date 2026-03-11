787eaaaaa40c7227fd7e224d5f7678ba821b073d
jhchoo135 <jhchoo135@gmail.com>
2026-03-11 17:46:53 +0800

    Comments updated on reconcile_manual_vs_KOHA.sql
    Added some comments for clarity

--- audit/reconcile_manual_vs_KOHA.sql
+++ audit/reconcile_manual_vs_KOHA.sql
@@ -28,13 +28,19 @@
 */
 
 SELECT 
-    -- If no data in ISBN field, data taken from ISSN field
-    -- If ISBN field is filled, data taken from ISBN field
+   
+    -- If no data in 'isbn' field, use data from 'issn' field
+    -- If 'isbn' field is filled, use data from 'isbn' field
     IF(bi.isbn IS NULL, bi.issn, bi.isbn) AS isbn_issn,
+   
     b.title AS title,
     b.abstract AS synopsis, 
-    -- The query below is because 'Author' is sometimes recorded in either
-    -- 'Non-Public Notes' or 'Public Notes' field
+   
+    -- The query below gets 'author' data from multiple possible fields
+    -- If 'author' field is filled, use data from 'author'
+    -- If 'author' field is not filled, check 'itemnotes_nonpublic'
+    -- If 'itemnotes_nonpublic' is filled, use data from 'itemnotes_nonpublic'
+    -- If 'itemnotes_nonpublic' is not filled, use data from 'itemnotes'
     IF(
         b.author IS NOT NULL, 
         b.author, 
@@ -44,14 +50,19 @@ SELECT
             i.itemnotes
         )
     ) AS author,
+   
     bi.publishercode AS publisher,
     b.copyrightdate AS publication_date,
     i.itemcallnumber AS DDC,
     i.barcode AS barcode
+   
 -- itemnotes_nonpublic, itemnotes, itemcallnumber, barcode are data taken from items
 FROM items i
+   
 -- title, abstract, author, copyrightdate are data taken from biblio
 LEFT JOIN biblio b USING (biblionumber)
+   
 -- isbn, issn, publishercode are taken from biblioitems
 LEFT JOIN biblioitems bi USING (biblionumber)
+   
 ORDER BY i.barcode
